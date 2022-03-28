3dd596f04bff9fccf71067b3cf2a7b755ba2d12e
andi.fivesco@gmail.com <andi.fivesco@gmail.com>
2022-03-28 09:49:07 +0700

    add API login

--- controllers/userController.js
+++ controllers/userController.js
@@ -38,6 +38,30 @@ const userController = {
         }
     },
     login:async (req,res) =>{
+        try {
+            const  {email,password} = req.body
+            const user = await Users.findOne({email})
+            if(!user) return res.status(400).json({msg:"user does not exists."})
+
+            //compare password using bcrypt
+            const isMatch = await bcrypt.compare(password,user.password)
+            if(!isMatch) return res.status(400).json({msg:"Incorret password."})
+
+            //login success
+            const accesstoken = createAccessToken({id:user._id})
+            const refreshtoken = createRefreshToken({id:user._id})
+            
+            res.cookie('refreshtoken',refreshtoken,{
+                httpOnly:true,
+                path:'/user/refresh_token'
+            })            
+            res.json({accesstoken})
+
+
+
+        } catch (err) {
+           return res.status(500).json({msg:err.message})             
+        }
         
     },
     refrehToken: async (req,res)=>{
