c31dc27f9af3318b95546d220efb6dba0550d5d3
andi.fivesco@gmail.com <andi.fivesco@gmail.com>
2022-03-28 10:03:36 +0700

    add router getUser

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,11 +2,15 @@ const router = require('express').Router()
 
 const userController = require('../controllers/userController')
 
+const auth = require('../middleware/auth')
+
 
 //user controller
 router.post("/register",userController.register)
 router.post("/login",userController.login)
 router.get("/refresh_token",userController.refrehToken)
+router.get('/info',auth,userController.getUser)
+
 
 
 
